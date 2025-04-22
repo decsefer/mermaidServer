a0ef24597252a5cda2709339e34dc2b625dcf8be
Sergio Rodriguez <sergiorod977@gmail.com>
2025-04-21 19:09:32 -0600

    cambios mermaid

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   webpack(config) {
     config.module.rules.push({
-      test: /chrome-aws-lambda/,
-      use: 'null-loader', // Ignora los archivos relacionados con chrome-aws-lambda
+
     });
 
     return config;
